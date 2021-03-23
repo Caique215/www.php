09552ed68143adc2552457488a245404acd84973
Caique <caiquesouzaresponsa@hotmail.com>
2021-03-22 21:36:43 -0300

    adicionando validação de campos do fórmulario

--- script.php
+++ script.php
@@ -5,42 +5,37 @@ $catetorias = [];
 $catetorias[]  = 'infantil';
 $catetorias[]  = 'adolescente';
 $catetorias[]  = 'adulto';
-$catetorias[]  = 'idoso';
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-var_dump($nome);
-var_dump($idade);
-
-
-
-
-
-/*if($idade >= 6 && $idade <= 12)
+if(empty($nome))
 {
-   for(si = 0; $i  <= count($categorias); $i++)
-   {
-       if($categorias[$i] == 'infantil')
-                 echo "O nadador " .$nome. "compete na categoria". $categorias[$i];
-   }
+      echo 'O nome não pode ser vazio';
+      return;
 }
-else if($idade >= 13 && $idade <= 18)
+if(strlen($nome) < 3)
 {
-    for(si = 0; $i  <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'adolescente')
-        echo "O nadador " .$nome. "compete na categoria adolescente";
-    }
+    echo '0 nome deve conter mais de 3 caracteres';
+  return;
 }
-else
-{
-    for(si = 0; $i  <= count($categorias); $i++)
-    {
-        if($categorias[$i] == 'adulto')
-             echo "O nadador " .$nome. "compete na categoria adulto";
 
-    }
-}*/
+if(strlen($nome) > 40)
+{
+    echo '0 nome é muito extenso';
+  return;
+
+
+}
+
+if(!is_numeric($idade))
+{
+    echo "informe um numero para idade";
+    return;
+}
+
+
+
+
 
 
