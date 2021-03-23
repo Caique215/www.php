66e8e151ccd45c2db64c4665a48ec527cbac26b0
Caique <caiquesouzaresponsa@hotmail.com>
2021-03-22 21:40:47 -0300

    adicionando validação de campos do fórmulario

--- script.php
+++ script.php
@@ -34,7 +34,31 @@ if(!is_numeric($idade))
     return;
 }
 
+if($idade >= 6 && $idade <= 12)
+{
+   for(si = 0; $i  <= count($categorias); $i++)
+   {
+       if($categorias[$i] == 'infantil')
+                 echo "O nadador " .$nome. "compete na categoria". $categorias[$i];
+   }
+}
+else if($idade >= 13 && $idade <= 18)
+{
+    for(si = 0; $i  <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adolescente')
+        echo "O nadador " .$nome. "compete na categoria adolescente";
+    }
+}
+else
+{
+    for(si = 0; $i  <= count($categorias); $i++)
+    {
+        if($categorias[$i] == 'adulto')
+             echo "O nadador " .$nome. "compete na categoria adulto";
 
+    }
+}
 
 
 
